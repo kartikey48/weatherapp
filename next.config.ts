304fee5930d6f6f28cf6fd350a88cd53e6136614
kartikey48 <ranakartik48@gmail.com>
2025-04-21 14:30:04 +0530

    "Removed unused import of 'next/image' from app/page.tsx"

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:"export",
+  output: 'export', // Required for static export
+  images: {
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  basePath: '/weatherapp', // OPTIONAL: if you're not using a custom domain
 };
 
 export default nextConfig;
