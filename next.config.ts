1f96c19cad4be4899fd65729c60d346378f9179a
Kartikey Singh Rana <102845091+kartikey48@users.noreply.github.com>
2025-04-22 13:11:19 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Required for static export
   eslint: {
     ignoreDuringBuilds: true, // Prevent ESLint errors from breaking the build
   },
